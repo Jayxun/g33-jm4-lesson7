c8415a8468a994fd80b3ca2158ab6140702388b2
Jayxunbey <muxammadovjayxun@gmail.com>
2023-10-25 14:59:55 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+
+       int stakan1=0;
+
+       long stakan2=2;
+
+       float kasrStakan=6.8f;
+
+       double doubleSoni=19.5;
+
+       char belgi=65;
+
+        int otkazma=(int) kasrStakan;
+        int doubleOtkazma = (int) doubleSoni;
+        System.out.println("otkazma = " + otkazma);
+        System.out.println("doubleOtkazma = " + doubleOtkazma);
+
+        String a="Jayxunbey";
+
+        //  byte = 1 -128...127
+
+        // short = 2 - 65536 = -32768...32767
+        // int = 4 - 4.2949 = -2.14...2.14
+        // long =  8-
+
+
+
+        //float = 4 -
+        // double = 8 -
+
+        // char 1
+
+        //boolean (true/false)
+
+
+
+
+    }
+}
